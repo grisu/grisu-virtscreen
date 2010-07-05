10941ea86b3e32cfd4d558cb36e75e36f38e3ea4
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2010-07-05 14:08:12 +1200

    Removed duplicate declarations in shell script.

--- src/main/resources/script.sh
+++ src/main/resources/script.sh
@@ -1,3 +1,6 @@
+export PVM_ROOT=/share/apps/gold/GOLD_Suite/GOLD/pvm3
+nohup /share/apps/gold/GOLD_Suite/GOLD/pvm3/lib/LINUX64/pvmd3  &
+
 GOLD_DIR=/share/apps/gold/GOLD_Suite
 #INDIR=/home/grid-bestgrid/Gold/Alpha_kcs_inter_6ga_20dvs
 INDIR=.
@@ -10,10 +13,6 @@ echo " PARALLEL OPTIONS" >> $INDIR/p110a_kcs_inter_6ga_20dvs.conf
 echo "hostfile = gold.hosts" >> $INDIR/p110a_kcs_inter_6ga_20dvs.conf
 
 cat $PBS_NODEFILE|python $INDIR/gold.py > $INDIR/gold.hosts
-
-$GOLD_DIR/bin/parallel_gold_auto 8 $INDIR/p110a_kcs_inter_6ga_20dvs.conf $INDIR/gold.hosts $INDIR
-GOLD_DIR=/share/apps/gold/GOLD_Suite
-INDIR=/home/grid-bestgrid/Gold/Alpha_kcs_inter_6ga_20dvs
 
 #clean up key files in the working directory
 rm -f $INDIR/Results/gold.pid
