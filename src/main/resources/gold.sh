baedff6640680eb1e928f981825d63f4244e5650
Yuriy Halytskyy <yuriy.halytskyy@gmail.com>
2010-08-02 19:49:52 +1200

    gold.sh should now compute number of ligands processed

--- src/main/resources/gold.sh
+++ src/main/resources/gold.sh
@@ -21,3 +21,8 @@ cat $PBS_NODEFILE|python $INDIR/gold.py > $INDIR/gold.hosts
 NO_OF_CPUS=$(cat $PBS_NODEFILE|wc -l)
 
 $GOLD_DIR/bin/parallel_gold_auto ${NO_OF_CPUS} $INDIR/$1 $INDIR/gold.hosts $INDIR
+
+# lets calculate number of components in processed library
+
+OUTDIR=$(grep 'directory =' /home/jfla018/grisu-dir/gold_s135261_fragsol_2010.07.27_16.47.916/*.conf|awk '{print $3}')
+cat $OUTDIR/gold.out|grep 'Ligand counter'|awk '{print $8}'|sort -n -u > $OUTDIR/number_of_ligands_in_library
