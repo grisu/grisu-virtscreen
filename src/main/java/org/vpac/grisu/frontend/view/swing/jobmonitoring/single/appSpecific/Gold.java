2168028b856ee3ff797c5c38905988ce183b1c88
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2010-08-18 16:33:19 +0200

    Added Gold specific viewer panel.

---
+++ src/main/java/org/vpac/grisu/frontend/view/swing/jobmonitoring/single/appSpecific/Gold.java
@@ -0,0 +1,22 @@
+package org.vpac.grisu.frontend.view.swing.jobmonitoring.single.appSpecific;
+
+import java.beans.PropertyChangeEvent;
+
+public class Gold extends AppSpecificViewerPanel {
+
+	public Gold() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void jobUpdated(PropertyChangeEvent evt) {
+
+	}
+
+}
