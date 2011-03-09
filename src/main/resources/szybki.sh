57dec4049675ec64d2ac586b9873da99928929df
Yuriy Halytskyy <yuriy.halytskyy@gmail.com>
2011-03-09 16:45:52 +1300

    cleaned up szybki shell script

--- src/main/resources/szybki.sh
+++ src/main/resources/szybki.sh
@@ -2,12 +2,19 @@
 
 date
 
+
 #echo 'quit'|/home/jfla018/Acsrc/Gold/start_pvm.sh &
 
+export SZYBKI_VERSION=1.5.2
+export SZYBKI_ARCH=redhat-RHEL-4-x64
 export OE_DIR=/share/apps/bio/openeye
+
 export OE_LICENSE=/share/apps/bio/openeye/oe_license.txt
-export PVM_PATH=/share/apps/bio/openeye/arch/redhat-RHEL4-x86/szybki/1.3.4/bin
-INDIR=/home/jfla018/Acsrc/Szybki/Crt_akr1c3
+
+export SZYBKI_PATH=${OE_DIR}/arch/${SZYBKI_ARCH}/${SZYBKI_VERSION}
+export PVM_PATH=${SZYBKI_PATH}/bin
+
+INDIR=`pwd`
 
 #clean up key files in the working directory
 #none
@@ -16,10 +23,10 @@ INDIR=/home/jfla018/Acsrc/Szybki/Crt_akr1c3
 cat $PBS_NODEFILE|python $INDIR/oe_pvm.py > $INDIR/optH_mmpbsa.conf
 
 # kill remainig pvm nodes first
-cat $PBS_NODEFILE|sort -u|xargs -I {} -n 1 ssh {} /home/jfla018/Acsrc/Szybki/Crt_akr1c3/halt-pvm.sh
+cat $PBS_NODEFILE|sort -u|xargs -I {} -n 1 ssh {} "bash halt-pvm.sh"
 
 #start Szybki job
-$OE_DIR/arch/redhat-RHEL4-x86/szybki/1.3.4/bin/szybki -param $INDIR/s3_snanalog_cmplt_mmpbopt_in.param -pvmconf $INDIR/optH_mmpbsa.conf
+${SZYBKI_PATH}/bin/szybki -param $INDIR/s3_snanalog_cmplt_mmpbopt_in.param -pvmconf $INDIR/optH_mmpbsa.conf
 
 date
 
